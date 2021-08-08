4bdb47494d16066b9105753be7336a1847dd762e
jackelinecargav <jackelinel.cardenasgavino@gmail.com>
2021-08-08 02:07:26 -0500

    row mapper

--- src/main/java/com/pe/estec/rowmapper/ComprobanteDetalleRowMapper.java
+++ src/main/java/com/pe/estec/rowmapper/ComprobanteDetalleRowMapper.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
 
-import com.pe.estec.model.Comprobante;
+
 import com.pe.estec.model.ComprobanteDetalle;
 
 public class ComprobanteDetalleRowMapper implements RowMapper<ComprobanteDetalle> {
@@ -13,6 +13,13 @@ public class ComprobanteDetalleRowMapper implements RowMapper<ComprobanteDetalle
 	@Override
 	public ComprobanteDetalle mapRow(ResultSet rs, int rowNum) throws SQLException {
 		ComprobanteDetalle comprobanteDetalle = new ComprobanteDetalle();
+		comprobanteDetalle.setId_comprobante_detalle(rs.getInt("id_comprobante_detalle"));
+		comprobanteDetalle.setId_comprobante(rs.getInt("id_comprobante"));
+		comprobanteDetalle.setCantidad(rs.getDouble("cantidad"));
+		comprobanteDetalle.setUnidad_medida(rs.getString("unidad_medida"));
+		comprobanteDetalle.setDescripcion(rs.getString("descripcion"));
+		comprobanteDetalle.setValor_unitario(rs.getDouble("valor_unitario"));
+		comprobanteDetalle.setIcbper(rs.getDouble("icbper"));
 		return comprobanteDetalle;
 	}
 }
